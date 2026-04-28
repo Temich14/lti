8e9a57620986fa072a8646a554b48f2c31766104
Erzumashev Artyom <temichkawaii@yandex.ru>
2026-04-28 08:10:00 +0700

    fix

---
+++ data/moodle/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'n8MeELvonvwBgShcc7C9ciVb9yOs7ZpElocalhost';
+$CFG->bootstraphash = '423d0e048c57498bee2ac4e55fe15afc';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
